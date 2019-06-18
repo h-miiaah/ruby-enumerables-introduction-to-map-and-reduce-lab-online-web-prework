fa5903cd43cb30a0151c5b7715f558435336f3c0
Haim D <haim11293@aol.com>
2019-06-18 00:02:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,11 +39,10 @@ def reduce_to_total(source_array, starting_point = 0)
   i = 0
   sum = 0
   while i < source_array.length do
-   source_array[i] += starting_point
    sum += source_array[i]
    i += 1
  end
- sum
+ sum += starting_point
 end
 
 def reduce_to_all_true(source_array)
